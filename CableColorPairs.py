7bf905a32925da347a8c5c1df583e2d4370fcb03
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-02-24 13:05:46 +0530

    Update CableColorPairs.py

--- CableColorPairs.py
+++ CableColorPairs.py
@@ -1,5 +1,3 @@
-MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
 def get_color_from_pair_number(pair_number):
     zero_based_pair_number = pair_number - 1
@@ -10,7 +8,6 @@ def get_color_from_pair_number(pair_number):
     if minor_index >= len(MINOR_COLORS):
         raise Exception('Minor index out of range')
     return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
-
 def get_pair_number_from_color(major_color, minor_color):
     try:
         major_index = MAJOR_COLORS.index(major_color)
