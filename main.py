75e632c557f521deed177eb6fc603cecb35a4142
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-02-24 11:42:38 +0530

    Updated unittests

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-
 import CableColorPairs
 
-def test_number_to_pair(pair_number,
-                        expected_major_color, expected_minor_color):
-  major_color, minor_color = get_color_from_pair_number(pair_number)
-  assert(major_color == expected_major_color)
-  assert(minor_color == expected_minor_color)
-  
-def test_pair_to_number(major_color, minor_color, expected_pair_number):
-  pair_number = get_pair_number_from_color(major_color, minor_color)
-  assert(pair_number == expected_pair_number)
+def test_number_to_pair(pair_number,expected_major_color, expected_minor_color):
+    major_color, minor_color = CableColorPairs.get_color_from_pair_number(pair_number)
+    assert(major_color == expected_major_color)
+    assert(minor_color == expected_minor_color)
 
+
+def test_pair_to_number(major_color, minor_color, expected_pair_number):
+    pair_number = CableColorPairs.get_pair_number_from_color(major_color, minor_color)
+    assert(pair_number == expected_pair_number)     
 
 if __name__ == '__main__':
-  test_number_to_pair(4, 'White', 'Brown')
-  test_number_to_pair(5, 'White', 'Slate')
-  test_pair_to_number('Black', 'Orange', 12)
-  test_pair_to_number('Violet', 'Slate', 25)
-  test_pair_to_number('Red', 'Orange', 7)
-  print('Done :)')
+    test_number_to_pair(4, 'White', 'Brown')
+    test_number_to_pair(5, 'White', 'Slate')
+    test_pair_to_number('Black', 'Orange', 12)
+    test_pair_to_number('Violet', 'Slate', 25)
+    test_pair_to_number('Red', 'Orange', 7)
+    print('Done :)')
