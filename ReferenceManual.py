a52c758b3b51843dee089bfec265ffde7094ba3a
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-02-24 13:00:11 +0530

    Update ReferenceManual.py

--- ReferenceManual.py
+++ ReferenceManual.py
@@ -5,6 +5,6 @@ def referenceManualCreation():
     number_end = 27     
 
     for colorPairNumber in range(number_start, number_end):
-        major_color, minor_color = CableColor_get_inputs.get_color_from_pair_number(colorPairNumber)
-        colorPairCodes = CableColor_get_inputs.color_pair_to_string(major_color, minor_color)
+        major_color, minor_color = CableColorPairs.get_color_from_pair_number(colorPairNumber)
+        colorPairCodes = CableColorPairs.color_pair_to_string(major_color, minor_color)
         print ("{:<25} {:<25} {:<25}".format(colorPairCodes, major_color, minor_color))
